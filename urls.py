8a6490245b54a2bf6f0181806bc0db4ec3fe0847
Joe Guo <joeg@catalyst.net.nz>
2016-07-13 15:39:55 +1200

    upgrade urls.py

--- urls.py
+++ urls.py
@@ -1,23 +1,10 @@
-from django.conf.urls.defaults import *
-
-# Uncomment the next two lines to enable the admin:
+from django.conf.urls import url, include
 from django.contrib import admin
+from django.contrib.auth.views import login
 admin.autodiscover()
 
-urlpatterns = patterns('',
-    # Example:
-    # (r'^idptest/', include('idptest.foo.urls')),
-
-    # Uncomment the admin/doc line below and add 'django.contrib.admindocs'
-    # to INSTALLED_APPS to enable admin documentation:
-    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    (r'^admin/', include(admin.site.urls)),
-
-    # Required for login:
-    (r'^accounts/login/$', 'django.contrib.auth.views.login'),
-
-    # URLs for the IDP:
-    (r'^idp/', include('saml2idp.urls')),
-)
+urlpatterns = [
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^idp/', include('saml2idp.urls')),
+    url(r'^accounts/login/$', login),
+]
