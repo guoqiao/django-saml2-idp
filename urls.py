d6c323e5e4ee5ce7b4e5e37a0d885575b89de502
Joe Guo <joeg@catalyst.net.nz>
2016-07-13 15:53:36 +1200

    upgrade urls

--- urls.py
+++ urls.py
@@ -1,7 +1,6 @@
 from django.conf.urls import url, include
 from django.contrib import admin
 from django.contrib.auth.views import login
-admin.autodiscover()
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
