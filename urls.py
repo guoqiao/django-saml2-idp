5e707b276a22cbb1086d4c3adcad82fbc7941eb9
Joe Guo <joeg@catalyst.net.nz>
2016-07-13 16:47:05 +1200

    make django admin the default site

--- urls.py
+++ urls.py
@@ -1,9 +1,7 @@
 from django.conf.urls import url, include
 from django.contrib import admin
-from django.contrib.auth.views import login
 
 urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
     url(r'^idp/', include('saml2idp.urls')),
-    url(r'^accounts/login/$', login, name='login'),
+    url(r'', include(admin.site.urls)),
 ]
